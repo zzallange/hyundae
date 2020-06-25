20ce1810177512c4385d83d031ff924bcc386ed9
zzallange <52703429+zzallange@users.noreply.github.com>
2020-06-25 15:22:53 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
     
+    //gnb
+    $('.submenu_bg').hover(function(){
+        $(this).siblings('a').toggleClass('active'); 
+    });
+    
+    $(function(){
+        //footer 사업자정보
+        $('.btn_corp').click(function(){
+            $('.corp_cont').toggleClass('on');       
+        }); 
+        //btn_top 상단으로
+        $('.btn_top').click(function(){
+            $('html, body').animate({scrollTop: 0}, 300);
+        });
+    });
+    //btn_top
     $(window).scroll(function() {
         if($(window).scrollTop() > $('.header').height()){
             $('.btn_top').addClass('on');
@@ -7,14 +23,6 @@ $(document).ready(function(){
             $('.btn_top').removeClass('on');
         }
     });
-    $(function(){
-        //footer
-        $('.btn_corp').click(function(){
-            $('.corp_cont').toggleClass('on');       
-        }); 
-        //top button
-        $('.btn_top').click(function(){
-            $('html, body').animate({scrollTop: 0}, 300);
-        });
-    });
+    
+    
 });//end
